821500fbad29b84d88bc47c66653a964385d69fa
Hero <gzoo@uzwelo.com>
2024-04-19 16:15:56 -0400

    Setup

---
+++ src/main/java/com/th3hero/EventBot/EventBotForDiscordApplication.java
@@ -0,0 +1,13 @@
+package com.th3hero.EventBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventBotForDiscordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventBotForDiscordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/th3hero/EventBot/EventBotForDiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.th3hero.EventBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBotForDiscordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
