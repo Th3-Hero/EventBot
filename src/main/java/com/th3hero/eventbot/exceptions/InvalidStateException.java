2fc8d7cabf4ca334d9222be93a03484a2e044fd9
Hero <gzoo@uzwelo.com>
2024-05-16 19:50:07 -0400

    Changes

--- src/main/java/com/th3hero/eventbot/exceptions/InvalidStateException.java
+++
@@ -1,7 +0,0 @@
-package com.th3hero.eventbot.exceptions;
-
-public class InvalidStateException extends RuntimeException {
-    public InvalidStateException(String message) {
-        super(message);
-    }
-}
